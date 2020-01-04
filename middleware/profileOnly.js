ba5dc903004b111618f90cb16f463d2b1df62ec4
Tiago Parente <t.parente.32@gmail.com>
2020-01-04 15:09:46 +0000

    bugfix endpoits do profile

--- middleware/profileOnly.js
+++ middleware/profileOnly.js
@@ -1,5 +1,5 @@
 export default function (context) {
-  if (!context.route.path.includes(context.store.state.auth.user.sub)) {
+  if (!context.route.path.includes(context.store.state.auth.user.sub) && !context.store.state.auth.user.groups.includes("Administrador")) {
     context.redirect("/401")
   }
 }
