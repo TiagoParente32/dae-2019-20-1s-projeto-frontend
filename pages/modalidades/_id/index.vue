622f9af8ab5f40d385ba1c081af8781581d93881
Henrique <2171300@my.ipleiria.pt>
2020-01-04 02:18:13 +0000

    sad life need to merge -.............-

--- pages/modalidades/_id/index.vue
+++ pages/modalidades/_id/index.vue
@@ -24,6 +24,8 @@
       <template v-slot:cell(actions)="row">
         <nuxt-link class="btn btn-primary btn-sm" :to="`/atletas/${row.item.username}`">Details</nuxt-link>
         <nuxt-link class="btn btn-primary btn-sm" :to="`/atletas/${row.item.username}/edit`">Edit</nuxt-link>
+        <input type="checkbox" id="checkboxAtletas" @click="checkAtleta(row.item.username)">
+        <label for="checkboxAtletas">Send Email</label>
       </template>
     </b-table>
     <p v-else>No Atletas Found.</p>
@@ -32,13 +34,28 @@
     <b-table v-if="treinadores.length" striped over :items="treinadores" :fields="userFields">
       <template v-slot:cell(actions)="row">
         <nuxt-link class="btn btn-primary btn-sm" :to="`/treinadores/${row.item.username}`">Details</nuxt-link>
-        <nuxt-link
-          class="btn btn-primary btn-sm"
-          :to="`/treinadores/${row.item.username}/edit`"
-        >Edit</nuxt-link>
+        <nuxt-link class="btn btn-primary btn-sm" :to="`/treinadores/${row.item.username}/edit`">Edit</nuxt-link>
+        <input type="checkbox" id="checkboxTreinadores" @click="checkTreinador(row.item.username)">
+        <label for="checkboxTreinadores">Send Email</label>
       </template>
     </b-table>
     <p v-else>No Treinadores Found.</p>
+    <div v-if="this.emailsToSendAtletas.length || this.emailsToSendTreinadores.length">
+      <h1>Send an E-mail to Selected Users</h1>
+      <form @submit.prevent="send">
+        <div>
+          <label for="subject">Subject:</label>
+          <b-form-input id="subject" v-model="subject"></b-form-input>
+        </div>
+        <div>
+          <label for="message">Message:</label>
+          <br>
+          <b-form-textarea rows="6" id="message" v-model="message" placeholder="add multiple lines"></b-form-textarea>
+        </div>
+        <hr>
+        <button class="btn btn-primary btn-sm" @click.prevent="send">SEND</button>
+      </form>
+    </div>
   </b-container>
 </template>
 <script>
@@ -50,7 +67,11 @@ export default {
       modalidade: {},
       horariosFields: ["id", "dia", "duracao", "horaInicio"],
       userFields: ["username", "name", "email", "actions"],
-      escaloesFields: ["id", "nome", "actions"]
+      escaloesFields: ["id", "nome", "actions"],
+      emailsToSendAtletas: [],
+      emailsToSendTreinadores: [],
+      subject: null,
+      message: null
     };
   },
   computed: {
@@ -74,6 +95,57 @@ export default {
     this.$axios
       .$get(`/api/modalidades/${this.id}`)
       .then(modalidade => (this.modalidade = modalidade || {}));
+  },
+  methods :{
+    checkTreinador(username) {
+      if (this.emailsToSendTreinadores.includes(username)) {
+        for (let i = 0; i < this.emailsToSendTreinadores.length; i++) {
+          if (this.emailsToSendTreinadores[i] === username) {
+            this.emailsToSendTreinadores.splice(i, 1);
+          }
+        }
+      } else {
+        this.emailsToSendTreinadores.push(username);
+      }
+    },
+    checkAtleta(username) {
+      if (this.emailsToSendAtletas.includes(username)) {
+        for (let i = 0; i < this.emailsToSendAtletas.length; i++) {
+          if (this.emailsToSendAtletas[i] === username) {
+            this.emailsToSendAtletas.splice(i, 1);
+          }
+        }
+      } else {
+        this.emailsToSendAtletas.push(username);
+      }
+    },
+    send() {
+      for (let i = 0; i < this.emailsToSendAtletas.length; i++) {
+        this.$axios.$post(`/api/atletas/${this.emailsToSendAtletas[i]}/email/send`, {
+          subject: this.subject,
+          message: this.message
+        })
+          .then(msg => {
+            this.$toast.success(msg)
+          })
+          .catch(error => {
+            this.$toast.error('Error sending the e-mail')
+          })
+      }
+      for (let i = 0; i < this.emailsToSendTreinadores.length; i++) {
+        this.$axios.$post(`/api/treinadores/${this.emailsToSendTreinadores[i]}/email/send`, {
+          subject: this.subject,
+          message: this.message
+        })
+          .then(msg => {
+            this.$toast.success(msg)
+          })
+          .catch(error => {
+            this.$toast.error('Error sending the e-mail')
+          })
+      }
+
+    }
   }
 };
 </script>
--- pages/pagamentos/index.vue
+++ pages/pagamentos/index.vue
@@ -5,20 +5,28 @@
       <h1>Pagamentos</h1>
       <b-table striped over :items="pagamentos" :fields="fields">
         <template v-slot:cell(actions)="row">
-          <nuxt-link class="btn btn-link" :to="`/pagamentos/${row.item.id}`">Details</nuxt-link>
-          <nuxt-link class="btn btn-primary" :to="`/pagamentos/${row.item.id}/edit`">Edit</nuxt-link>
+          <nuxt-link class="btn btn-primary btn-sm" :to="`/pagamentos/${row.item.id}`">Details</nuxt-link>
+          <nuxt-link class="btn btn-primary btn-sm" :to="`/pagamentos/${row.item.id}/edit`">Edit</nuxt-link>
           <button
             type="button"
             class="btn btn-danger btn-sm"
             @click.prevent="deletePagamento(row.item.id)"
           >Delete</button>
+          <b-form-checkbox @change="check(row.item)" v-if="row.item.estado === 'PAGO'">Generate PDF</b-form-checkbox>
         </template>
       </b-table>
-      <nuxt-link to="/pagamentos/create" class="btn btn link btn-primary">Create a Pagamento</nuxt-link>
+      <nuxt-link to="/pagamentos/create" class="btn btn-primary">Create a Pagamento</nuxt-link>
+      <b-button v-if="this.pagamentosToPDF.length" v-on:click.prevent="generatePDF">Generate PDF With Selected Pagamentos</b-button>
     </b-container>&emsp;
   </div>
+
 </template>
 <script>
+
+  //<input type="checkbox" class="custom-control-input" id="checkbox" @click="check(row.item)" v-if="row.item.estado == 'PAGO'">
+  //<label class="custom-control-label" v-if="row.item.estado == 'PAGO'" for="checkbox">Generate PDF</label>
+  import jsPDF from 'jspdf';
+  import 'jspdf-autotable';
 export default {
   middleware: "adminOnly",
 
@@ -34,7 +42,8 @@ export default {
         "dataLancamento",
         "actions"
       ],
-      pagamentos: []
+      pagamentos: [],
+      pagamentosToPDF: []
     };
   },
   methods: {
@@ -65,6 +74,30 @@ export default {
           // handle error
           console.log(error);
         });
+    },
+    check(pagamento){
+      if(this.pagamentosToPDF.includes(pagamento)){
+        for(let i = 0; i< this.pagamentosToPDF.length; i++){
+          if (this.pagamentosToPDF[i] === pagamento){
+            this.pagamentosToPDF.splice(i,1);
+          }
+        }
+      }else{
+        this.pagamentosToPDF.push(pagamento);
+      }
+    },
+    generatePDF(){
+      if(this.pagamentosToPDF.length){
+        var doc = new jsPDF();
+        doc.text('Recibos', 10, 10);
+
+        doc.autoTable({
+          foot:[["Data Lançamento","Estado","ID","Preço Final","ID Produto","Quantidade","Username"]],
+          body: this.pagamentosToPDF
+        });
+
+        doc.save('recibos.pdf');
+      }
     }
   },
   created() {
